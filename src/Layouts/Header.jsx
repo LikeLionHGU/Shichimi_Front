a0e1e604312f28f6849b465cd3521d8c014ea57c
playoh <wazoski05@gmail.com>
2025-08-08 12:32:11 +0900

    feat: 헤더 경계 및 로고 버튼 생성

--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -3,15 +3,30 @@ import {Link, NavLink } from "react-router-dom";
 import styled from "styled-components";
 import { themeColors  } from "../assets/styles/StyledComponents";
 
+const NoCenterHorizontal = styled.div`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  background-color: ${themeColors.gray.color};
+  height: 84px;
+`;
+
+const LogoImg = styled.button`
+  display : flex;
+  align-items : center;
+  margin: 0 0 0 88px;
+`;
 
 
 const Header = () => {
   return(
     <>
       <header>
-        <div>
-          <Link to='/'>Logo</Link>
-        </div>
+        <NoCenterHorizontal>
+          <LogoImg>
+            <Link to='/'>Logo</Link>
+          </LogoImg>
+        </NoCenterHorizontal>
       </header>
     </>
   );
