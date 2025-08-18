36ba10b3bfc90ffda6a91a071e65600cca52c471
playoh <wazoski05@gmail.com>
2025-08-19 00:43:33 +0900

    feat: api history fin

--- src/pages/StoreDetail.jsx
+++ src/pages/StoreDetail.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import { GlobalStyle, themeColors } from "../assets/styles/StyledComponents";
+import {useParams} from "react-router-dom";
 
 import NextDoor_Board from "../components/Detail_NextDoor";
 import Tops_Board from "../components/Details_Tops";
@@ -9,6 +10,7 @@ import Visit_Board from "../components/Details_Visit_Board";
 import crab from "../assets/images/Frame 35.svg";
 
 import Ex from "../assets/images/Back01.svg";
+import { getMarketInfo } from "../server/apis/api";
 
 const TotalPage = styled.div`
   display: flex;
@@ -69,24 +71,59 @@ const StoreTitle_right_name = styled.div`
 
   font-size: 1.4vw;
   border-radius: 0 15px 15px 0;
-  background-color: ${themeColors.blue.color};
+  background-color: ${({ $color }) => $color };   
   color: ${themeColors.white.color};
   border: none;
 `;
 
 function StoreDetail(){
+  const {marketId} = useParams();
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(null);
 
-  // const data = await getMarketInfo(marketId);
+  useEffect(() => {
+    let alive = true;
+    (async() => {
+      try{
+        const res = await getMarketInfo(marketId);
+        
+        if(!alive) return
+          setData(res);
+      }catch(e){
+        console.error("API 호출 실패:",e?.message, e?.response?.data);
+        if(alive) setErr("가게의 상세 정보를 불러오지 못했습니다.");
+      }finally{
+        if(alive) setLoading(false);
+      }
+    })();
+    return () => {alive = false};
+  }, [marketId]);
 
+  if (loading) return <>불러오는 중...</>;
+  if (err)     return <>{err}</>;
+
+  const {
+    name= "",
+    phoneNumber= "",
+    address="",
+    openTime= "",
+    marketLogo="",
+    foodMenuImg="",
+    color = themeColors.blue?.color,
+  } = data ?? {};
+
+  console.log('api color:', color);
 
   return(
     <>
       <TotalPage>
         <Detail_Left>
           <StoreTitle>
-            <StoreTitle_left_icon><img src={crab} alt="ICON" /></StoreTitle_left_icon>
-            <StoreTitle_right_name>포항대게</StoreTitle_right_name>
+            <StoreTitle_left_icon><img src={marketLogo} alt="ICON" /></StoreTitle_left_icon>
+            <StoreTitle_right_name $color={color}>{name}</StoreTitle_right_name>
           </StoreTitle>
+          
           <Hist_Board />
           <Tops_Board />
         </Detail_Left>
